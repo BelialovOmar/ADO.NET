7717b34ae249e8acb09624daf47329a7436b76b3
Admin <Admin@LAPTOP>
2023-04-04 12:37:16 +0300

    Connected

---
+++ ADO_Lesson1/ADO-Lesson1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;  // не забути NuGet
+using System.Data;
+using System.IO;
+
+
+namespace ADO_Lesson1
+{
+	public partial class MainWindow: Window
+	{
+		// об'єкт-підключення, основа ADO
+		private SqlConnection _connection;  // MS SQL ADO
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			// !! створення об'єкта не відкиває підключення
+			_connection = new();
+
+			// головний параметр - рядок підключення
+			_connection.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Admin\source\repos\BelialovOmar\ADO.NET\ADO_Lesson1\ADO-Lesson1\Database1.mdf;Integrated Security=True";
+		}
+		
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			try  // відкриваємо підключення
+			{
+				_connection.Open();
+				// відображаємо статус підключення на вікні
+				StatusConnection.Content = "Connected";
+				StatusConnection.Foreground = Brushes.Green;
+			}
+			catch (SqlException ex)
+			{
+				// відображаємо статус підключення на вікні
+				StatusConnection.Content = "Disconnected";
+				StatusConnection.Foreground = Brushes.Red;
+
+				MessageBox.Show(ex.Message);
+				this.Close();
+			}
+		}
+		
+		private void Window_Closed(object sender, EventArgs e)
+		{
+			if (_connection?.State == ConnectionState.Open)
+			{
+				_connection.Close();
+			}
+
+		}
+	}	
+}
