6f991c9af0ca4311124b380cbb11645e94043667
Admin <Admin@LAPTOP>
2023-04-05 07:32:48 +0300

    Portal

---
+++ ADO_Lesson1/ADO-Lesson1/Entity/Department.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_Lesson1.Entity
+{
+	public class Department
+	{
+		public Guid Id
+		{
+			get; set;
+		}            // відбраження поля Id	 UNIQUEIDENTIFIER
+		public String Name
+		{
+			get; set;
+		}        // відбраження поля Name NVARCHAR(50)
+		public DateTime? DeleteDt
+		{
+			get; set;
+		} // ! типи БД та мови як правило відрізняються
+
+		public Department()
+		{
+			Id = Guid.NewGuid();
+			Name = null!;
+		}
+
+		public Department(DbDataReader reader)
+		{
+			Id = reader.GetGuid(0);
+			Name = reader.GetString(1);
+			DeleteDt = reader.IsDBNull(2)
+						? null
+						: reader.GetDateTime(2);
+		}
+
+
+	}
+}
